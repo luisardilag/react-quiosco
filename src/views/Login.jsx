43a00555af86ef9b4eb35793d9dd10b67b8554a3
Luis Ardila <luisardilag@gmail.com>
2023-11-29 18:14:37 -0300

    update: Incorpora Imagenes y Routing

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,3 +1,52 @@
+import { Link } from "react-router-dom";
+
 export default function login() {
-  return <div>login</div>;
+  return (
+    <>
+      <h1 className="text-4xl font-black">Iniciar Sesión</h1>
+      <p>Para crear un pedido, inicia Sesión</p>
+
+      <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+        <form action="">
+          <div className="mb-5">
+            <label htmlFor="email" className="text-slate-800">
+              Email:
+            </label>
+            <input
+              id="name"
+              type="email"
+              name="email"
+              className="border border-gray-200 rounded-md w-full p-3"
+              placeholder="Email"
+            />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="password" className="text-slate-800">
+              Password:
+            </label>
+            <input
+              id="name"
+              type="password"
+              name="password"
+              className="border border-gray-200 rounded-md w-full p-3"
+              placeholder="Password"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Iniciar Sesión"
+            className="bg-indigo-600 hover:bg-indigo-800 w-full uppercase mt-5 p-3 pointer text-white font-bold"
+          />
+        </form>
+      </div>
+
+      <nav className="mt-5">
+        <Link to="/auth/registro">
+          ¿No tienes cuenta? <span className="text-indigo-600">Regístrate</span>
+        </Link>
+      </nav>
+    </>
+  );
 }
--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -1,10 +1,80 @@
 import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Registro() {
   return (
-    <div>
-      layout
-      <Outlet />
-    </div>
+    <>
+      <h1 className="text-4xl font-black">Crea tu cuenta</h1>
+      <p>Crea tu cuenta llenando el formulario</p>
+
+      <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+        <form action="">
+          <div className="mb-5">
+            <label htmlFor="name" className="text-slate-800">
+              Nombre:
+            </label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              className="border border-gray-200 rounded-md w-full p-3"
+              placeholder="Nombre"
+            />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="email" className="text-slate-800">
+              Email:
+            </label>
+            <input
+              id="name"
+              type="email"
+              name="email"
+              className="border border-gray-200 rounded-md w-full p-3"
+              placeholder="Email"
+            />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="password" className="text-slate-800">
+              Password:
+            </label>
+            <input
+              id="name"
+              type="password"
+              name="password"
+              className="border border-gray-200 rounded-md w-full p-3"
+              placeholder="Password"
+            />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="password_confirmation" className="text-slate-800">
+              Repetir Password:
+            </label>
+            <input
+              id="name"
+              type="password"
+              name="password"
+              className="border border-gray-200 rounded-md w-full p-3"
+              placeholder="Password"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Crear cuenta"
+            className="bg-indigo-600 hover:bg-indigo-800 w-full uppercase mt-5 p-3 pointer text-white font-bold"
+          />
+        </form>
+      </div>
+
+      <nav className="mt-5">
+        <Link to="/auth/login">
+          ¿Ya tienes cuenta?
+          <span className="text-indigo-600"> Inicia Sesión</span>
+        </Link>
+      </nav>
+    </>
   );
 }
