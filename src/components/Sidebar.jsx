76250be7f88de141eae72c384aea80635f45c6f6
Luis Ardila <luisardilag@gmail.com>
2023-11-30 09:59:22 -0300

    update: Categorias y sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import Categorias from "./Categorias";
+import { categorias } from "../data/categorias";
+
+export default function Sidebar() {
+  return (
+    <aside className="md:w-72">
+      <div className="p-4">
+        <img className="w-40" src="img/logo.svg" alt="logo" />
+      </div>
+
+      <div className="mt-10">
+        {categorias.map((categoria) => (
+          <Categorias categoria={categoria} />
+        ))}
+      </div>
+
+      <div className="my-5 px-5">
+        <button
+          type="button"
+          className="text-center bg-red-500 w-full p-3 font-bold text-white truncate"
+        >
+          Cancelar Orden
+        </button>
+      </div>
+    </aside>
+  );
+}
